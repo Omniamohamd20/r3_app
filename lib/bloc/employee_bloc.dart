c3692923b08e2063b07c849106f3676413836858
omnia <omnia.mohamed.elgezar@gmail.com>
2024-08-04 17:41:55 +0300

    commit

--- lib/bloc/employee_bloc.dart
+++ lib/bloc/employee_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:r3_app1/models/employee.dart';
@@ -9,14 +10,17 @@ part 'employee_state.dart';
 class EmployeeBloc extends Bloc<EmployeeEvent, EmployeeState> {
   final EmployeeRepository _employeeRepository;
   EmployeeBloc(this._employeeRepository) : super(EmployeeLadingState()) {
-    print('>>>>>>>>>bloc init');
-    on<EmployeeEvent>((event, emit) {
-      // emit(EmployeeLadingState());
-      _employeeRepository.getEmployees().then((employees) {
-        emit(EmployeeLoadedState(employees!));
-      }).catchError((error) {
-        emit(EmployeeErrorState(error.toString()));
-      });
+    // print('>>>>>>>>>bloc init');
+    on<LoadEmployeeEvent>(loadEmployee);
+  }
+
+  FutureOr<void> loadEmployee(
+    LoadEmployeeEvent event,Emitter<EmployeeState> emit) async{
+    emit(EmployeeLadingState());
+   await _employeeRepository.getEmployees().then((employees) {
+      emit(EmployeeLoadedState(employees!));
+    }).catchError((error) {
+      emit(EmployeeErrorState(error.toString()));
     });
   }
 }
